8b7dc910ddedd704d10d074728acfe0b70f279f1
Dean Heins <deanheins12@gmail.com>
2022-10-06 16:11:12 -0400

    Made more files and changed names

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ document.querySelector('button');
 
 button.addEventListener('click',function(){
     if(confirm('Do you want to go back a page?')){
-        window.open('https://)
+        window.open('https://google.com')
     }
 })
