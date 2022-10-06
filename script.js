04e1e5e667e16d53765e32384047ccede1186275
Dean Heins <deanheins12@gmail.com>
2022-10-06 15:29:32 -0400

    More things

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-var button =null;
+var back=
+document.querySelector('button');
+
+button.addEventListener('click',function(){
+    if(confirm('Do you want to go back a page?')){
+        window.open('https://)
+    }
+})
