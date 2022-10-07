5744e77256aae4418dded1ace735566dc7e2cb91
Dean Heins <deanheins12@gmail.com>
2022-10-07 07:18:06 -0400

    made updates

--- introduction/index.js
+++ introduction/index.js
@@ -1,9 +1,9 @@
 var anchor1 =
-document.querySelector('a1');
+document.querySelector('a');
 
 anchor1.addEventListener('click',function(){
     alert('next page')
 
-    window.open ('')
+    window.open ('https://deanheins.github.io/RichmondHillBooks/next/')
 }
 )
