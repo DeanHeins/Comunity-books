0df421880b4796bd9568619d656e21fa78679843
URMOM <116090729+BlackHearted610@users.noreply.github.com>
2023-02-22 15:33:33 -0500

    kick it

--- login-page/login.js
+++ login-page/login.js
@@ -1,9 +1,16 @@
+//adds a variable (var for short) and saves the form in it...
 var form = document.querySelector("#login_form");
+
+//stuff that includes adding a variable and makes a list of user emails that will be accepted...
 var users = [
-    'deanheins12@gmail.com',
-    'deanheins11@outlook.com',
+    {
+        username: 'Uncle Bedless',
+        email: 'deanheins12@gmail.com',
+        password: 'pass1234'
+    }
 ];
 
+//add's an event listener that will prevent the default application and switch to another on next rows.
 form.addEventListener("submit", function(event){
     event.preventDefault();
     
@@ -19,6 +26,12 @@ form.addEventListener("submit", function(event){
         alert('NO ACCESS, LEAVE MY DOMAIN TRESPASER 😠 ');
         return false;
     }
+
+    
+    
+    // Find the user object that has an email equal to the value of the email input field
+    // If a user exists, we need to compare the password in the password field with the user's password
+    // If the passwords do not match, we return false and error.
 
 
       alert("Succesful login! :)");
